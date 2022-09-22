c1d937f59ae1be335852b7a15a07937f8b427445
Sarah Campbell <sarahcampbell4646@gmail.com>
2022-09-22 16:34:53 -0400

    replace with for loop

--- eqArrays.js
+++ eqArrays.js
@@ -7,11 +7,31 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(arr1, arr2) {
-  return Array.isArray(arr1) && Array.isArray(arr2) && arr1.length === arr2.length && arr1.every((val, index) => val === arr2[index]);
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
 };
+
+
+
 
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); //true
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); //false
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); //true
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); //false
+
+
+
+
+
+
+
+
+//return Array.isArray(arr1) && Array.isArray(arr2) && arr1.length === arr2.length && arr1.every((val, index) => val === arr2[index]);
