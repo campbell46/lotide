7f703969a1dfb1731195807c69770414ef28eb29
Sarah Campbell <sarahcampbell4646@gmail.com>
2022-09-22 16:36:18 -0400

    cleaned up code, added comments

--- eqArrays.js
+++ eqArrays.js
@@ -7,9 +7,11 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(arr1, arr2) {
+  //return false if arrays are not same length
   if (arr1.length !== arr2.length) {
     return false;
   }
+  //return false if array values do not equal
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
       return false;
@@ -18,20 +20,7 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
-
-
-
-
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); //true
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); //false
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); //true
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); //false
-
-
-
-
-
-
-
-
-//return Array.isArray(arr1) && Array.isArray(arr2) && arr1.length === arr2.length && arr1.every((val, index) => val === arr2[index]);
