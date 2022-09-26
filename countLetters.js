999ecdf5162ac3c9b668aec1ac352fdbea8c4294
Sarah Campbell <sarahcampbell4646@gmail.com>
2022-09-26 15:55:43 -0400

    added comments

--- countLetters.js
+++ countLetters.js
@@ -8,12 +8,16 @@ const assertEqual = function(actual, expected) {
 
 const countLetters = function(sentence) {
   let results = {};
+  //loop through each letter in sentence
   for (const letter of sentence) {
+    //if letter is a space, continue without doing anything
     if (letter === " ") {
       continue;
     }
+    //if results already has this property counter plus 1
     if (results[letter]) {
       results[letter] += 1;
+      //if results does not have this property add it with the count of 1
     } else {
       results[letter] = 1;
     }
