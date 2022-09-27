1111b508afccba70df15733cdd097038cb807bb8
Sarah Campbell <sarahcampbell4646@gmail.com>
2022-09-27 12:50:31 -0400

    added findKeyByValue

---
+++ findKeyByValue.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passsed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object, value) {
+  let genre;
+  //loop through object keys, checking if value is a match
+  for (const key in object) {
+    if (bestTVShowsByGenre[key] === value) {
+      genre = key;
+    }
+  }
+  return genre;
+};
+
+const bestTVShowsByGenre = {
+  sciFi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sciFi");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn 9-9"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
