2e526747727d6274460f5ce3dce25cd943ac3cbe
Sarah Campbell <sarahcampbell4646@gmail.com>
2022-09-28 18:56:12 -0400

    added map

---
+++ map.js
@@ -0,0 +1,35 @@
+const eqArrays = function(arr1, arr2) {
+  //return false if arrays are not same length
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  //return false if array values do not equal
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passsed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+let words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+assertArraysEqual(map(words, word => word[0]), ['g', 'c', 't', 'm', 't']);
+assertArraysEqual(map(words, word => word.indexOf('o') + 1), [3, 2, 2, 4, 2]);
+assertArraysEqual(map(words, word => word.toUpperCase()), ['GROUND', 'CONTROL', 'TO', 'MAJOR', 'TOM']);
