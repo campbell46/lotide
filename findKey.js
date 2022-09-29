11e98176b10bb9e63ec6d6942dcea595ba68baa8
Sarah Campbell <sarahcampbell4646@gmail.com>
2022-09-28 20:30:41 -0400

    add findKey.js

---
+++ findKey.js
@@ -0,0 +1,20 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passsed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(object, callback) {
+  for (const item in object) {
+    //if item.model equals 'wrangler' return item
+    if (callback(object[item])) {
+      return item;
+    }
+  }
+};
+  
+assertEqual(findKey({ "Blue Hill": { stars: 1 }, "Akaleri": { stars: 3 }, "noma": { stars: 2 },"elBulli": { stars: 3 }, "Ora": { stars: 2 }, "Akelarre": { stars: 3 }}, x => x.stars === 2), 'noma');
+
+assertEqual(findKey({ "Mazda": {model: 'CX-5'}, "Honda": {model: 'Civic'}, "Ford": {model: 'F150'}, "Jeep": {model: 'wrangler'}, "Chevrolet": {model: 'Equinox'}}, x => x.model === 'wrangler'), 'Jeep');
