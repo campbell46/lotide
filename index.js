757f63bb08c5777411a6de686b7d61f75eb6e280
Sarah Campbell <sarahcampbell4646@gmail.com>
2022-10-10 12:45:13 -0400

    added index

---
+++ index.js
@@ -0,0 +1,27 @@
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+const countOnly = require('./countOnly');
+const countLetters = require('./countLetters');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
+const letterPosition = require('./letterPosition');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./require');
+
+module.exports = {
+  head: head,
+  tail: tail,
+  middle: middle,
+  countOnly: countOnly,
+  countLetters: countLetters,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+  flatten: flatten,
+  letterPosition: letterPosition,
+  map: map,
+  takeUntil: takeUntil,
+  without: without
+};
