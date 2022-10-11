34463d997db86e9422a018d60480ddce3160be14
Sarah Campbell <sarahcampbell4646@gmail.com>
2022-10-10 20:59:33 -0400

    added assertObjectsEqualTest

---
+++ test/assertObjectsEqualTest.js
@@ -0,0 +1,4 @@
+const assertObjectsEqual = require('../assertObjectsEqual');
+
+//test function, check if objects are equal
+assertObjectsEqual({school: "Lighthouse", year: 2022, city: "Ottawa"}, {school: "Lighthouse", year: 2022, city: "Ottawa"});
