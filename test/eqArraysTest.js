ead389b599a0be3447dd411a1db21480b841d9af
Sarah Campbell <sarahcampbell4646@gmail.com>
2022-10-10 21:15:59 -0400

    added more tests

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -3,10 +3,13 @@ const _ = require('../index');
 
 //test function, if 2 arrays are equal
 describe("#eqArrays", () => {
-  it("return true if arrays match", () => {
+  it("returns true if arrays match", () => {
     assert.strictEqual(_.eqArrays([1,2,3], [1,2,3]), true);
   });
-  it("return true if arrays match(array of arrays)", () => {
-    assert.strictEqual(_.eqArrays(_.flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]), true);
+  it("returns true if arrays match(array with inner array)", () => {
+    assert.strictEqual(_.eqArrays([[2, 3], [4]], [[2, 3], [4]], [[2, 3], [4]], [[2, 3], [4]]), true);
+  });
+  it("returns true if arrays match(array with multiple inner arrays)", () => {
+    assert.strictEqual(_.eqArrays([[1, [2, [3]]], [4,[5]]], [[1, [2, [3]]], [4,[5]]]), true);
   });
 });
