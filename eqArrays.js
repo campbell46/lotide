3eefd24792e508352b822ada1710a67d40434cdc
Sarah Campbell <sarahcampbell4646@gmail.com>
2022-10-10 21:15:40 -0400

    refactored file with recursion for Stretch

--- eqArrays.js
+++ eqArrays.js
@@ -6,7 +6,10 @@ const eqArrays = function(arr1, arr2) {
   }
   //return false if array values do not equal
   for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
+    //if array is an array, use recursion to compare inner arrays
+    if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+      return eqArrays(arr1[i], arr2[i]);
+    } else if (arr1[i] !== arr2[i]) {
       return false;
     }
   }
