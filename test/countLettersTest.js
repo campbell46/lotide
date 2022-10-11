ca19caeae2d76271c3a765c286575f69dd5512ff
Sarah Campbell <sarahcampbell4646@gmail.com>
2022-10-10 22:14:08 -0400

    fixed spelling error

--- test/countLettersTest.js
+++ test/countLettersTest.js
@@ -3,7 +3,7 @@ const _ = require('../index');
 
 //test function, returns object of letter counts
 describe("#countLetters", () => {
-  it("return letter count of character in string", () => {
+  it("returns letter count of character in string", () => {
     assert.deepEqual(_.countLetters("lighthouse in the house"), {l: 1, i: 2, g: 1, h: 4, t: 2, o: 2, u: 2, s: 2, e: 3, n: 1});
   });
 });
